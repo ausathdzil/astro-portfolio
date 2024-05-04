417c734dd3a645cf24abeff0bff99ee130045124
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-05-04 14:31:40 +0700

    feat: Add Footer component to home page layout

--- src/components/ProjectTabs.tsx
+++ src/components/ProjectTabs.tsx
@@ -1,28 +1,36 @@
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function ProjectTabs() {
   return (
-    <Tabs defaultValue="bookshelf" className="w-full">
-      <TabsList className="grid w-full grid-cols-3">
+    <Tabs
+      defaultValue="bookshelf"
+      className="w-[100%] text-base"
+    >
+      <TabsList className="grid w-full grid-cols-2 gap-2">
         <TabsTrigger value="bookshelf">Bookshelf</TabsTrigger>
         <TabsTrigger value="calculator">Calculator</TabsTrigger>
-        <TabsTrigger value="other-works">Other Works</TabsTrigger>
       </TabsList>
       <TabsContent value="bookshelf">
-        Bookshelf app made with vanilla JavaScript.
+        <div className="grid w-full grid-cols-2">
+          <div></div>
+          <p>
+            Bookshelf app made with vanilla JavaScript. This was my
+            <strong>first ever</strong> mini project stepping into the front-end
+            dev world, it's basically a <i>todo app</i>. I want to rebuild
+            this app with a full stack framework soon, we'll see how it goes ;).
+          </p>
+        </div>
       </TabsContent>
       <TabsContent value="calculator">
-        Soon to be made Calculator app, 
-        I intend to build it with <strong>React</strong>. Here's a preview of the UI I made in figma.
-      </TabsContent>
-      <TabsContent value="other-works">
-        Right now I'm working with KSM Android UPNVJ on an internal project, we're building a LMS.
+        <div className="grid w-full grid-cols-2">
+          <div></div>
+          <p>
+            Soon to be made Calculator app, I intend to build it with{" "}
+            <strong>React</strong>. This is only a preview of the UI I made in
+            figma.
+          </p>
+        </div>
       </TabsContent>
     </Tabs>
-  )
+  );
 }
