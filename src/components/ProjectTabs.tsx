40dd36ed782821ac3747f7090073d51ecbec7909
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-05-04 14:45:27 +0700

    feat: Update Footer component styling and add social media links

--- src/components/ProjectTabs.tsx
+++ src/components/ProjectTabs.tsx
@@ -6,24 +6,24 @@ export default function ProjectTabs() {
       defaultValue="bookshelf"
       className="w-[100%] text-base"
     >
-      <TabsList className="grid w-full grid-cols-2 gap-2">
+      <TabsList className="grid w-full grid-cols-2 gap-4">
         <TabsTrigger value="bookshelf">Bookshelf</TabsTrigger>
         <TabsTrigger value="calculator">Calculator</TabsTrigger>
       </TabsList>
       <TabsContent value="bookshelf">
-        <div className="grid w-full grid-cols-2">
-          <div></div>
+        <div className="grid w-full grid-rows-2 sm:grid-cols-2 gap-4">
+          <div className="h-24"></div>
           <p>
             Bookshelf app made with vanilla JavaScript. This was my
             <strong>first ever</strong> mini project stepping into the front-end
-            dev world, it's basically a <i>todo app</i>. I want to rebuild
-            this app with a full stack framework soon, we'll see how it goes ;).
+            dev world, it's basically a <i>todo app</i>. I want to rebuild this
+            app with a full stack framework soon, we'll see how it goes ;).
           </p>
         </div>
       </TabsContent>
       <TabsContent value="calculator">
-        <div className="grid w-full grid-cols-2">
-          <div></div>
+        <div className="grid w-full grid-rows-2 sm:grid-cols-2 gap-4">
+          <div className="h-24"></div>
           <p>
             Soon to be made Calculator app, I intend to build it with{" "}
             <strong>React</strong>. This is only a preview of the UI I made in
