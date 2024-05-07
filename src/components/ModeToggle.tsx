3040ff928e4e36ef9359ae272bccc8a883677a0b
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-05-07 19:38:59 +0700

    style: Adjusted nav stylings

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const [theme, setThemeState] = useState<"theme-light" | "dark">(
-    "theme-light"
+    "dark"
   );
 
   useEffect(() => {
@@ -26,8 +26,14 @@ export function ModeToggle() {
         size="icon"
         className="group"
       >
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 group-hover:fill-zinc-950" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 group-hover:fill-zinc-100" />
+        <Sun 
+          className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 
+          dark:scale-0 group-hover:fill-zinc-950" 
+        />
+        <Moon 
+          className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all 
+          dark:rotate-0 dark:scale-100 group-hover:fill-zinc-100" 
+        />
         <span className="sr-only">Toggle theme</span>
       </Button>
     </div>
