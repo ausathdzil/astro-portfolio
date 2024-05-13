266c2f5559e05a707f8655ae059e35b3daf352ab
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-05-13 23:50:13 +0700

    Add new projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -7,9 +7,9 @@ export interface Project {
 export function getProjects(): Project[] {
   return [
     {
-      title: "Bookshelf",
-      link: "https://github.com/ausathdzil/bookshelf-app",
-      description: "Bookshelf app made with vanilla JavaScript.",
+      title: "React Calculator",
+      link: "https://github.com/ausathdzil/react-calculator",
+      description: "Calculator app made with React and Tailwind CSS.",
     },
     {
       title: "Social Linktree",
@@ -17,9 +17,25 @@ export function getProjects(): Project[] {
       description: "A simple social linktree made with React and Tailwind CSS.",
     },
     {
+      title: "Discord Classroom Bot",
+      link: "https://github.com/ausathdzil/discord-blast-bot",
+      description:
+        "Discord bot for personal classroom use. Made with Discord js.",
+    },
+    {
+      title: "Bookshelf",
+      link: "https://github.com/ausathdzil/bookshelf-app",
+      description: "Bookshelf app made with vanilla JavaScript.",
+    },
+    {
       title: "Terminal Quiz",
       link: "https://github.com/ausathdzil/quiz-app",
       description: "A quiz game that runs on your terminal, made with C.",
+    },
+    {
+      title: "Music Gallery",
+      link: "https://github.com/ausathdzil/astro-portfolio",
+      description: "My personal music gallery, it's on this website :)",
     },
   ];
 }
