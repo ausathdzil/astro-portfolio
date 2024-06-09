8c57a2d3bb9d975e1f0f98410689169bded9c81e
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-06-09 14:34:36 +0700

    feat: Add preview link for projects if there's any

--- src/components/ProjectsAccordion.tsx
+++ src/components/ProjectsAccordion.tsx
@@ -3,8 +3,8 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import { type Project, getProjects } from "@/lib/projects";
+} from '@/components/ui/accordion';
+import { type Project, getProjects } from '@/lib/projects';
 
 const projects: Project[] = getProjects();
 
@@ -20,27 +20,50 @@ function chunkArray(array: Project[], chunkSize: number) {
 const projectChunks = chunkArray(projects, 3);
 
 export default function ProjectsAccordion() {
-
   return (
     <>
       {projectChunks.map((chunk, chunkIndex) => (
-        <div key={chunkIndex} className="flex flex-col lg:flex-row gap-8 my-8 lg:my-0">
+        <div
+          key={chunkIndex}
+          className="flex flex-col lg:flex-row gap-8 my-8 lg:my-0"
+        >
           {chunk.map((project, index) => (
-            <Accordion key={index} type="single" collapsible className="w-full">
-              <AccordionItem value={`item-${index + 1}`} className="md:mb-8">
+            <Accordion
+              key={index}
+              type="single"
+              collapsible
+              className="w-full"
+            >
+              <AccordionItem
+                value={`item-${index + 1}`}
+                className="md:mb-8"
+              >
                 <AccordionTrigger>{project.title}</AccordionTrigger>
 
                 <AccordionContent>
                   {project.description}
-                  
-                  <a 
-                    href={project.link} 
-                    target="_blank"
-                    className="underline underline-offset-8 transition ease-in-out hover:decoration-zinc-500"
-                    aria-label={`link to ${project.title} github repo`}
-                  >
-                    <p className="mt-4">github repo 📂</p>
-                  </a>
+
+                  <div className="flex gap-4 justify-start items-start">
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      className="underline underline-offset-8 transition ease-in-out hover:decoration-zinc-500"
+                      aria-label={`link to ${project.title} github repo`}
+                    >
+                      <p className="mt-4">github repo 📂</p>
+                    </a>
+
+                    {project.webLink && (
+                      <a
+                        href={project.webLink}
+                        target="_blank"
+                        className="underline underline-offset-8 transition ease-in-out hover:decoration-zinc-500"
+                        aria-label={`link to ${project.title}`}
+                      >
+                        <p className="mt-4">preview 🔗</p>
+                      </a>
+                    )}
+                  </div>
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,7 @@
 export interface Project {
   title: string;
   link: string;
+  webLink?: string;
   description: string;
 }
 
@@ -9,36 +10,36 @@ export function getProjects(): Project[] {
     {
       title: 'NASA APODs Gallery',
       link: 'https://github.com/ausathdzil/nasa-apods-gallery',
-      description:
-        'My attempt at cloning NASA APOD.',
+      webLink: 'https://nasa-apods-gallery.vercel.app/',
+      description: 'My attempt at cloning NASA APOD.',
     },
     {
       title: 'Bookshelf',
       link: 'https://github.com/ausathdzil/nextjs-bookshelf',
-      description:
-        'A catalogue for managing books.',
+      description: 'A catalogue for managing books.',
     },
     {
       title: 'Portfolio',
       link: 'https://github.com/ausathdzil/astro-portfolio',
+      webLink: 'https://ausathdzil.vercel.app/',
       description: 'My personal web portfolio.',
     },
     {
       title: 'React Calculator',
       link: 'https://github.com/ausathdzil/react-calculator',
+      webLink: 'https://react-calculator-ausath.vercel.app/',
       description: 'Calculator web app.',
     },
     {
-      title: 'Social Linktree',
+      title: 'Social Links Profile',
       link: 'https://github.com/ausathdzil/social-links',
-      description:
-        'A simple social linktree.',
+      webLink: 'https://social-links-ausath.vercel.app/',
+      description: 'A simple social links for your profile.',
     },
     {
       title: 'Discord Classroom Bot',
       link: 'https://github.com/ausathdzil/discord-blast-bot',
-      description:
-        'Discord bot for personal classroom use. Made with discord.js.',
+      description: 'Discord bot for personal classroom use.',
     },
   ];
 }
